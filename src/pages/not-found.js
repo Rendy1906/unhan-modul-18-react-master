33a7a3bfa09dd12967721f1aee805377de36d798
Rendy1906 <rendycahya18@gmail.com>
2022-11-22 00:27:15 +0700

    Initial commit

---
+++ src/pages/not-found.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// Modifikasi halaman berikut untuk menjadi halaman 404 yang menarik!
+const NotFound = () => {
+  return (
+    <div class='error'>4 0 4<span class="tab">N O T</span><span class="tab">F O U N D</span></div>
+  )
+}
+
+export default NotFound
